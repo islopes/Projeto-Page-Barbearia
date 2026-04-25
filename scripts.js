12d0a6f6a997de2e7388ad10091557ed1caf007f
seu-username <seu-email>
2026-04-25 17:47:56 -0300

    Animaçoês - nossos serviços e galeria

--- scripts.js
+++ scripts.js
@@ -1,6 +1,24 @@
 const menuBtn = document.querySelector('.menu-btn'); /*vou no html e mapeo a class mencionada*/
 const navBar = document.querySelector(".navbar");
 
+const myObserver = new IntersectionObserver ((entries) => {
+   entries.forEach((entry) =>{
+     if (entry.isIntersecting){
+        entry.target.classList.add('aparecer')
+     } else {
+        entry.target.classList.remove('aparecer')
+     }
+   })
+})
+
+const elements = document.querySelectorAll(".box-container")
+
+elements.forEach((element) => myObserver.observe(element))
+
+
+
+
+
 menuBtn.addEventListener('click', () => {
 navBar.classList.toggle('active'); /*quando clicar, ele vai adicionar a classe active, e se clicar novamente, ele vai remover a classe active*/
 }) /*adiciono um evento de click, e quando clicar, ele vai executar a função*/
